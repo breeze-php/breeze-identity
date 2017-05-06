7c35b197ff5264bf4b8585d628b672c0c121c070
Suh Du <suhdu@protonmail.com>
2017-05-06 11:18:53 -0600

    Add module.php

--- src/Module.php
+++ src/Module.php
@@ -2,20 +2,16 @@
 
 namespace Breeze\Identity;
 
-use Core\Module\StandardAutoloaderTrait;
-
 /**
  * Class Module
  */
 class Module
 {
-    use StandardAutoloaderTrait;
-
     /**
      * @return mixed
      */
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '../config/module.config.php';
     }
 }
